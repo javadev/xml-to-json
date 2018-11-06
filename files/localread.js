9fe0734bab7ece7a20e55de1c9bff5a683b3e1de
javadev <javadev75@gmail.com>
2018-11-06 05:53:02 +0200

    Improve javascript

--- files/localread.js
+++ files/localread.js
@@ -1,21 +1,17 @@
-// Copyright 2013 Data Design Group, Inc  All Rights Reserved
 function ieReadLocalFile(that,callback,encoding) {
-        //alert(that.value);
         if(!that.value)return;
         if(that.value.length<=0)return;
         var request;
-        if (window.XMLHttpRequest && false) { // code for IE7+, Firefox, Chrome, Opera, Safari
+        if (window.XMLHttpRequest && false) {
            request=new XMLHttpRequest();
         }
-        else {// code for IE6, IE5
-          request=new ActiveXObject("Msxml2.XMLHTTP"); // Microsoft.XMLHTTP
+        else {
+          request=new ActiveXObject("Msxml2.XMLHTTP");
         }
         var fn=that.value;
-        //fn="file:///"+that.value.replace("\\","/");
         request.open('get', fn, true);
         request.onreadystatechange = function() 
         {
-          //alert(request.readystate+":"+request.status);
           if (request.readyState == 4 && (request.status == 200 || request.status==0)) {
               callback(request.responseText);
           }
@@ -30,19 +26,17 @@ function readLocalFile(that,callback,encoding)
     if(that.files && that.files[0]){
 	     var reader = new FileReader();
 	     reader.onload = function (e) {  
-           //document.getElementById(txtTargetName).value=e.target.result;
            callback(e.target.result);
-	     };//end onload()
+	     };
        reader.readAsText(that.files[0],encoding);
-    }//
-} // readLocaFile
+    }
+}
 
 function loadTextFile(f,callback)
 {
     var encoding=null;
-    if(document && document.getElementById("txtEncoding")) { // add support for encoding
+    if(document && document.getElementById("txtEncoding")) {
         encoding=document.getElementById("txtEncoding").value;
     }
     (window.FileReader ? readLocalFile(f, callback, encoding) : ieReadLocalFile(f, callback, encoding));
-    //(navigator.appName.search('Microsoft') > -1) ? ieReadLocalFile(f, callback, encoding) : readLocalFile(f, callback, encoding);
 }
